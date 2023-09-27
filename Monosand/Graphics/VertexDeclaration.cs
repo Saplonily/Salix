af5c07b354e7977b35cb3fb71b44d76602541c6b
Saplonily <Saplonily@outlook.com>
2023-09-27 23:09:54 +0800

    adjust `VertexDeclaration`

--- Monosand/Graphics/VertexDeclaration.cs
+++ Monosand/Graphics/VertexDeclaration.cs
@@ -1,11 +1,37 @@
 ﻿namespace Monosand;
 
-public class VertexDeclaration
+public class VertexDeclaration : IEquatable<VertexDeclaration>
 {
-    public int Count => Attributes.Length;
+    private int? hash;
+    private VertexElementType[] attr;
 
-    public VertexElementType[] Attributes { get; }
+    public int Count => Attributes.Length;
+    public ReadOnlySpan<VertexElementType> Attributes { get => new(attr); }
 
     public VertexDeclaration(params VertexElementType[] attributes)
-        => Attributes = attributes;
+        => attr = attributes;
+
+    public bool Equals(VertexDeclaration? other)
+        => other is not null && attr.SequenceEqual(other.attr);
+
+    public override bool Equals(object? other)
+        => other is VertexDeclaration vd && attr.SequenceEqual(vd.attr);
+
+    public override int GetHashCode()
+    {
+        if (hash is not null)
+            return hash.Value;
+        HashCode hc = new();
+        foreach (var item in attr)
+            hc.Add(item);
+        int result = hc.ToHashCode();
+        hash = result;
+        return result;
+    }
+
+    public static bool operator ==(VertexDeclaration? left, VertexDeclaration? right)
+        => left is not null && left.Equals(right);
+
+    public static bool operator !=(VertexDeclaration? left, VertexDeclaration? right)
+        => !(left == right);
 }
