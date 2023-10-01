1babb93cb6381ee95613f12293b6ef5494934948
Saplonily <Saplonily@outlook.com>
2023-10-01 22:07:02 +0800

    Add comments to 'ThrowHelper'

--- Monosand/ThrowHelper.cs
+++ Monosand/ThrowHelper.cs
@@ -5,22 +5,26 @@ namespace Monosand;
 
 public static class ThrowHelper
 {
+    /// <summary>Throws <see cref="ObjectDisposedException"/> when <paramref name="condition"/> is true.</summary>
     public static void ThrowIfDisposed([DoesNotReturnIf(true)] bool condition, object? instance)
     {
         if (condition) throw new ObjectDisposedException(instance?.GetType().FullName);
     }
 
+    /// <summary>Throws <see cref="ArgumentNullException"/> when <paramref name="argument"/> is null.</summary>
     public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
         if (argument is null) throw new ArgumentNullException(paramName);
     }
 
+    /// <summary>Throws <see cref="ArgumentOutOfRangeException"/> when <paramref name="argument"/> is negative.</summary>
     public static void ThrowIfNegative(int argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
         if (argument < 0)
             throw new ArgumentOutOfRangeException(paramName, "Can't be negative.");
     }
 
+    /// <summary>Throws <see cref="InvalidOperationException"/> when <paramref name="condition"/> is true.</summary>
     public static void ThrowIfInvalid([DoesNotReturnIf(true)] bool condition, string? msg = null)
     {
         if (condition) throw new InvalidOperationException(msg);
